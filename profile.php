17f39c62944fcab6e7bfd1c1d85264424de6ff9a
Theresa Calangian <theresacalangian@Admins-MacBook-Pro.local>
2020-10-02 23:12:52 +0800

    Added profile.php

---
+++ profile.php
@@ -0,0 +1,65 @@
+
+
+<!DOCTYPE html>
+<html>
+
+<?php
+
+session_start();
+// Redirect to login page if not logged in
+if (!isset($_SESSION['loggedin'])) {
+	header('Location: ../index.html');
+	exit;
+}
+
+
+$con = mysqli_connect('localhost', 'root', '', 'phplogin');
+if (mysqli_connect_error()) {
+	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+
+// Retrieves data from database
+$stmt = $con->prepare('SELECT password, email FROM accounts WHERE id = ?');
+$stmt->bind_param('i', $_SESSION['id']);
+$stmt->execute();
+$stmt->bind_result($password, $email);
+$stmt->fetch();
+$stmt->close();
+?>
+
+	<head>
+		<meta charset="utf-8">
+		<title>Profile Page</title>
+		<link href="style.css" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+	</head>
+	<body class="loggedin">
+		<nav class="navtop">
+			<div>
+				<h1>CMSC 128.1</h1>
+				<a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
+				<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
+			</div>
+		</nav>
+		<div class="content">
+			<h2>Profile Page</h2>
+			<div>
+				<p>Your account details are below:</p>
+				<table>
+					<tr>
+						<td>Username:</td>
+						<td><?=$_SESSION['name']?></td>
+					</tr>
+					<tr>
+						<td>Password:</td>
+						<td><?=$password?></td>
+					</tr>
+					<tr>
+						<td>Email:</td>
+						<td><?=$email?></td>
+					</tr>
+				</table>
+			</div>
+		</div>
+	</body>
+</html>
